e7680dd0b7f14fd1eb69ceff00c64573099eb003
Jonathan Hinchliffe <jhinchliffe42@gmail.com>
2026-08-05 19:27:29 +0100

    border collision for cubes mostly done

--- CubeGame.py
+++ CubeGame.py
@@ -8,14 +8,31 @@ canvasHeight = 720
 canvas = Canvas(window, width=canvasWidth, height=canvasHeight, bg="white" )
 canvas.pack()
 
-vel = GameObjects.Velocity(20, 91)
-cube = GameObjects.Cube(size = 20, position = dict(x=500, y=100),velocity=vel)
+
+#goes top right
+vel1 = GameObjects.Velocity(10, 45)
+cube1 = GameObjects.Cube(size = 20, position = dict(x=500, y=100),velocity=vel1)
+
+#goes bottom right
+vel2 = GameObjects.Velocity(10, 135)
+cube2 = GameObjects.Cube(size = 20, position = dict(x=500, y=100),velocity=vel2, colour="blue")
+
+#bottom left
+vel3 = GameObjects.Velocity(10, 190)
+cube3 = GameObjects.Cube(size = 20, position = dict(x=500, y=100),velocity=vel3, colour="red")
+
+#top right
+vel4 = GameObjects.Velocity(10, 315)
+cube4 = GameObjects.Cube(size = 20, position = dict(x=500, y=100),velocity=vel4, colour="green")
+
 border = GameObjects.Border(canvasWidth, canvasHeight)
+cubes = [cube1,cube2,cube3,cube4]
+
 
 def updateCanvas():
     canvas.delete("all")
-    cube.position_update([border])
-    cube.render(canvas)
+    cube3.frame_update([border])
+    cube3.render(canvas)
     #print("render complete")
     window.after(100, updateCanvas)
 
--- GameObjects.py
+++ GameObjects.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 import math
+from re import A
 
 class Velocity():
 
@@ -13,24 +14,38 @@ class Velocity():
     def calculate_x(self):
         if self.angle == 0 or self.angle == 180:
             return 0
+        elif self.angle == 90:
+            return self.speed
+        elif self.angle == 270:
+            return -self.speed
         else:
-            x = self.speed * (math.sin((math.radians(self.angle%90))))
+            if (0 < self.angle < 90) or (180 < self.angle < 270):
+                x = self.speed * (math.sin((math.radians(self.angle%90))))
+            elif (90 < self.angle < 180) or (270 < self.angle < 360):
+                x = self.speed * (math.cos(math.radians(self.angle%90)))
             if self.angle > 180:
-                x = -x
+                x -= x
             return round(x,2)
         
     def calculate_y(self):
         if self.angle == 90 or self.angle == 270:
             return 0
+        elif self.angle == 0:
+            return -self.speed
+        elif self.angle == 180:
+            return self.speed
         else:
-            y = self.speed * (math.cos(math.radians(self.angle%90)))
-            if 90 < self.angle < 270:
+            if (90 < self.angle < 180) or (270 < self.angle < 360):
+                y = self.speed * (math.sin(math.radians(self.angle%90)))
+            elif (0 < self.angle < 90) or (180 < self.angle < 270):
+                y = self.speed * (math.cos(math.radians(self.angle%90)))
+            if 90 > self.angle or self.angle > 270:
                 y = -y
             return round(y,2)
 
     def update_components(self):
-        self.calculate_x()
-        self.calculate_y()
+        self.x = self.calculate_x()
+        self.y = self.calculate_y()
 
     def set_speed(self, speed):
         self.speed = speed
@@ -43,7 +58,7 @@ class Velocity():
 class Game_Object(ABC):
     
     @abstractmethod
-    def position_update(self):
+    def frame_update(self):
         pass
 
     @abstractmethod
@@ -79,10 +94,13 @@ class Collision_Rectangle(Collision_Model):
 
     def collision_check(self, objects = []):
         self.calculate_outline()
+        collisions = []
         for o in objects:
             if isinstance(o, Border):
-                self.border_collision_check(o)
+                border_collisions = self.border_collision_check(o)
+                    #there has been a collision
         #print(self.edges)
+        return (border_collisions, collisions)
     
     def border_collision_check(self,o):
         pass    
@@ -95,8 +113,15 @@ class Cube(Game_Object, Collision_Rectangle):
         self.position = position
         self.velocity = velocity
         self.colour = colour
+        self.can_bounce = True
 
-    def position_update(self, objects = [],frame_rate = 30):
+    def frame_update(self, objects = [], frame_rate = 30):
+        self.position_update()
+        collisions = self.collision_check(objects)
+        if type(collisions[0]) != bool and self.can_bounce:
+            self.bounce(collisions[0])
+
+    def position_update(self,frame_rate = 30):
         ## TODO
         ## need collision method
         ##some way for framerate to become independent of velocity
@@ -104,22 +129,89 @@ class Cube(Game_Object, Collision_Rectangle):
 
         self.position["x"] = round(self.position["x"] + (self.velocity.x),2)
         self.position["y"] = round(self.position["y"] + (self.velocity.y),2)
-        self.collision_check(objects)
-        #print(self.position)
+        
+
+    def bounce(self, collisions):
+        #print("BOUNCE!", collisions)
+        #left right top bottom
+        #T/F   T/F  T/F  T/F
+        if (collisions[0] == True or collisions[1] == True) and (collisions[2] or collisions[3]):
+            #corner bounce
+            self.velocity.set_angle( round((self.velocity.angle+180)%360))
+            self.position_update()
+            return
+        
+        if self.velocity.angle == 0 or self.velocity.angle == 90 or self.velocity.angle == 180 or self.velocity.angle == 270:
+            #tangent bounce
+            self.velocity.set_angle(round((self.velocity.angle+180)%360))
+            self.position_update()
+            return
+        
+        if collisions[0]:
+            #print(self.velocity.angle, "Before bounce")
+            #left bounce
+            #angle is between 181 and 359 but not 270
+            if self.velocity.angle > 270:
+                self.velocity.set_angle(round((self.velocity.angle - (180 + (2*(self.velocity.angle-180))))%360))
+            if self.velocity.angle < 270:
+                new_angle = round((180-(2*(90-(360-self.velocity.angle))))-(360-self.velocity.angle))
+                print(new_angle)
+                self.velocity.set_angle(new_angle)
+
+            #print(self.velocity.angle, "After bounce")
+            self.position_update()
+            return
+
+        if collisions[1]:
+            #right bounce
+            #angle is between 1 and 179 but not 90
+            #print(self.velocity.angle, "Before bounce")
+            if self.velocity.angle > 90:
+                self.velocity.set_angle(round(self.velocity.angle + (180 - (2*(90-(180-self.velocity.angle))))))
+            if self.velocity.angle < 90:
+                self.velocity.set_angle(round(360-((180-(2*(90-self.velocity.angle))) -self.velocity.angle)))
+            self.position_update()
+            #print(self.velocity.angle, "After bounce")
+        
+        if collisions[2]:
+            #top bounce
+            #angle is betweem 1 and 90 or 270 and 360
+            if self.velocity.angle < 90:
+                self.velocity.set_angle(round(self.velocity.angle+(180-(2*(180-(90-self.velocity.angle))))))
+            if self.velocity.angle > 270:
+                self.velocity.set_angle(round(360-((360-self.velocity.angle)+(180-(2*(90-(90-(360-self.velocity.angle))))))))
+            self.position_update()
+            return
+        
+        if collisions[3]:
+            #bottom bounce
+            #angle is between 91 and 269 but not 180
+            if self.velocity.angle < 180:
+                self.velocity.set_angle(round(self.velocity.angle-(180-(180-(2*(90-(180-self.velocity.angle)))))))
+            if self.velocity.angle > 180:
+                self.velocity.set_angle(round((180-(2*(90-(self.velocity.angle-180))))+self.velocity.angle))
+            self.position_update()
+            return
 
     def border_collision_check(self, o):
-        if self.vertices[0][0] < 0 and self.vertices[0][1] > 0 and self.vertices[2][0] < 0 and self.vertices[2][1] < o.height:
-            #left wall bounce
-            if self.velocity.angle < 270:
-                self.velocity.angle -= 90
-            else:
-                self.velocity.angle = (self.velocity+90)%360
-        #top bounce        
-        elif self.vertices[0][0] > 0 and self.vertices[0][1] < 0 and self.vertices[1][0] < o.width and self.vertices[1][1] < 0:
-            if self.velocity.angle < 90:
-                self.velocity.angle += 90
-            else:
-                self.velocity.angle -= 90
+        left_collision = False
+        right_collision = False
+        top_collision = False
+        bottom_collision = False
+        if self.vertices[0][0] <= 0:
+            left_collision = True
+        if self.vertices[3][0] >= o.width:
+            right_collision = True
+        if self.vertices[0][1] <= 0:
+            top_collision = True
+        if self.vertices[3][1] >= o.height:
+            bottom_collision = True
+
+        #if there has been a collision return which sides collided
+        if left_collision == True or right_collision == True or top_collision ==True or bottom_collision == True:
+            return (left_collision, right_collision, top_collision,bottom_collision)
+        else:
+            return (False)
 
     def render(self, canvas):
         canvas.create_rectangle(self.position["x"]-(self.size//2), self.position["y"]-(self.size//2), self.position["x"]+(self.size//2), self.position["y"]+(self.size//2), fill=self.colour)
